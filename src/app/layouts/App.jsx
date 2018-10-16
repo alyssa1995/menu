cca105e288cff942ed1a93a1f5420a1999966b67
Alyssa Durante <alyssadurante@Whats-iMac.fios-router.home>
2018-10-16 17:55:41 -0400

    5:55

--- src/app/layouts/App.jsx
+++ src/app/layouts/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import { Button } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/eventDashboard/eventDashboard'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div class="app">
         <h1>Menu</h1>
+          <EventDashboard></EventDashboard>
       </div>
     );
   }
